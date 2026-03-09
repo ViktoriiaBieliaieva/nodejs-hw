a663b2e50603b3325210a621b91c0f84275c8fe8
Viktoriia <viktoriiabieliaieva91@gmail.com>
2026-03-09 14:24:17 +0100

    done hw

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises';
-
-// без кодування
-const buffer = await fs.readFile('file.txt');
-console.log(buffer); // <Buffer ... >
-
-// з кодуванням
-const data = await fs.readFile('file.txt', 'utf8');
-console.log('Вміст файлу:', data); // "Hello"
