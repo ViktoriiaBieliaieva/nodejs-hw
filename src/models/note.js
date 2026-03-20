6962706e850fc73cd288d4a03d7ca98bcb0ca417
Viktoriia <viktoriiabieliaieva91@gmail.com>
2026-03-20 14:01:11 +0100

    done hw

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,5 @@
-import { Schema } from 'mongoose';
-import { model } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import { TAGS } from '../contacts/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -15,18 +15,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -35,5 +24,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
