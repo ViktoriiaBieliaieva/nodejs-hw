bad65ed396b03263edf85ebfdd636200e6840791
Viktoriia <viktoriiabieliaieva91@gmail.com>
2026-04-02 13:50:39 +0200

    email reset

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -101,7 +101,7 @@ export const requestResetEmail = async (req, res, next) => {
   const user = await User.findOne({ email });
   if (!user) {
     return res.status(200).json({
-      message: 'If this email exists, a reset link has been sent',
+      message: 'Password reset email sent successfully',
     });
   }
 
@@ -134,7 +134,7 @@ export const requestResetEmail = async (req, res, next) => {
   }
 
   res.status(200).json({
-    message: 'If this email exists, a reset link has been sent',
+    message: 'Password reset email sent successfully',
   });
 };
 
@@ -159,6 +159,6 @@ export const resetPassword = async (req, res) => {
   await Session.deleteMany({ userId: user._id });
 
   res.status(200).json({
-    message: 'Password reset successfully. Please log in again.',
+    message: 'Password reset successfully',
   });
 };
