a683ef05f2d47d4ab1c5c3aa4d837a6944e9aa2a
Viktoriia <viktoriiabieliaieva91@gmail.com>
2026-04-02 13:33:47 +0200

    add reset email

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,13 @@ import {
   logoutUser,
   refreshUserSession,
   requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
   requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -23,6 +25,11 @@ router.post(
   '/auth/request-reset-email',
   celebrate(requestResetEmailSchema),
   requestResetEmail,
+);
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
 );
 
 export default router;
